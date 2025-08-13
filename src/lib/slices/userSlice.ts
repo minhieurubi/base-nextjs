eebca6dcf22bbc31ab6189aeb08ebd80e1f183df
Minh Hieu Trinh <hieutm@rubicontechno.com>
2025-08-13 17:14:48 +0700

    add redux

---
+++ src/lib/slices/userSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserInfo } from "@/types/common";
+
+const DEFAULT_INFO = {
+  id: "",
+  email: "",
+  username: "",
+  role: "",
+  urlAvatar: "",
+  password: '',
+};
+
+interface UserState {
+  info: UserInfo;
+}
+
+const initialState: UserState = {
+  info: DEFAULT_INFO,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUserInfo: (state, action: PayloadAction<UserInfo>) => {
+      state.info = action.payload;
+    },
+    clearUserInfo: (state) => {
+      state.info = DEFAULT_INFO;
+    },
+  },
+});
+
+export const { setUserInfo, clearUserInfo } = userSlice.actions;
+export default userSlice.reducer;
